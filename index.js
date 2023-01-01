1cda2ac0b3ce1e32243dcd884f27a4ffb593c099
El_Tigere <eltigere@web.de>
2023-01-01 20:46:35 +0100

    http response is now loaded from a file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 8080;
 const host = 'localhost';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('test');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    content = fs.readFileSync('page\\index.html');
+    res.write(content);
     res.end();
 });
 
