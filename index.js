9c5a807e99434faa94662b8f6723a751ad0c95be
El_Tigere <eltigere@web.de>
2023-01-02 20:05:05 +0100

    - any file in the page folder can be accessed - added test icon and stylesheet - added page map

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const fs = require('fs');
 const port = 8080;
 const host = 'localhost';
 
+const pageMap = JSON.parse(fs.readFileSync('pageMap.json'));
+const mimeTypes = JSON.parse(fs.readFileSync('mimeTypes.json'));
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    content = fs.readFileSync('page\\index.html');
+    
+    // respond with 404 to all requests with special chars in the url (except / and . (but not ..))
+    const url = (((req.url || '/').match(/^([\w\d/]\.?)+$/g) || [''])[0].toLowerCase());
+    if(url == '') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    
+    // get the requested page name or the default page name
+    let page = url;
+    if(pageMap[page]) page = pageMap[page];
+    if(!fs.existsSync('page' + page)) page = pageMap['default'];
+    
+    // get the mime type
+    const pageType = mimeTypes[(page.match(/\..+$/g) || ['default'])[0]] || mimeTypes['default'];
+    
+    // respond with the page content
+    res.writeHead(200, {'Content-Type': [pageType]});
+    const content = fs.readFileSync('page' + page);
     res.write(content);
     res.end();
+    
 });
 
 server.listen(port, host, () => {
